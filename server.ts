3ffe7e8297ea3a0cc03dbacbb91b8f4fcc52eb27
Dominick Harasimiuk <dominick.harasimiuk@gmail.com>
2021-01-25 19:13:25 -0600

    heroku deploy preparations

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express =  require('express');
 import cors = require('cors');
-import dotenv = require('dotenv')
+import dotenv = require('dotenv');
+import path = require('path');
 
 dotenv.config();
 const app = express();
@@ -8,8 +9,13 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "build")))
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+// app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
