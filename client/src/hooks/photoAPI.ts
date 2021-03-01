18e897c5f4542fc9ab0a1dc52a8d32713908babd
Dominick Harasimiuk <dominick.harasimiuk@gmail.com>
2021-02-28 22:46:21 -0500

    basic camera functionality

--- client/src/hooks/photoAPI.ts
+++ client/src/hooks/photoAPI.ts
@@ -1,8 +1,25 @@
-import { useCamera } from "@ionic/react-hooks/camera";
+import { Plugins, CameraResultType, CameraSource,
+    CameraPhoto, Capacitor } from '@capacitor/core';
+
+const { Camera } = Plugins;
 
 export function photoAPI() {
 
-    const takePhoto = () => {}
+    const takePhoto = async () => {
+        try{
+            const cameraPhoto = await Camera.getPhoto({
+                allowEditing: false,
+                resultType: CameraResultType.Uri,
+                source: CameraSource.Camera,
+                quality: 100
+            });
+
+            return cameraPhoto;
+
+        } catch (e) {
+            console.log("No Photo (likely cancelled by user)");
+        }
+    }
 
     return {
         takePhoto
